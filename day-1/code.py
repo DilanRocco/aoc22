9f718c3a5da3f2a50fc1d87956132dc2e6649daa
DilanRocco <captaindilan12@gmail.com>
2022-12-01 20:05:45 -0500

    day one

--- day-1/code.py
+++ day-1/code.py
@@ -1,7 +1,6 @@
+all_cals,reindeer_cals = [], 0
 with open("input.txt") as f:
     t = f.read().split("\n")
-
-all_cals,reindeer_cals = [], 0
 for x in t: 
     if (x == ''):
         all_cals.append(reindeer_cals)
@@ -13,8 +12,9 @@ all_cals.sort(reverse=True)
 
 print("Part One")
 print(all_cals[0])
+
 print("")
 
 print("Part Two")
-print(all_cals[0] + all_cals[1] + all_cals[2])
+print(sum(all_cals[:3]))
 
